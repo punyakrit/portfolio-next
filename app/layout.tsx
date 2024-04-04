4853606b5b46a119b21906758e2316143d752949
Punyakrit <punyakritsinghmakhni@gmail.com>
2024-04-04 23:51:58 +0530

    ad

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1567175925424561"
+     crossOrigin="anonymous"></script>
         <title>Punyakrit's Portfolio Website</title>
         <meta
           name="description"
